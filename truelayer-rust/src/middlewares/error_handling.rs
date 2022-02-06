94a9a27ccce4a5a7aed6ac240de5ac27f7a01bed
Marco Cameriero <marco.cameriero@truelayer.com>
2022-02-06 15:51:18 +0100

    Update legacy errors mapping.

--- truelayer-rust/src/middlewares/error_handling.rs
+++ truelayer-rust/src/middlewares/error_handling.rs
@@ -1,4 +1,7 @@
-use crate::error::{ApiError, Error};
+use crate::{
+    common::TL_CORRELATION_ID_HEADER,
+    error::{ApiError, Error},
+};
 use async_trait::async_trait;
 use reqwest::{Request, Response};
 use reqwest_middleware::{Middleware, Next};
@@ -20,40 +23,22 @@ impl Middleware for ErrorHandlingMiddleware {
         // Capture the response
         let response = next.run(req, extensions).await?;
 
-        // Build an error if the response is not a success.
-        // Try parsing the contents of the error as an `ErrorResponse`,
-        // but if that doesn't work, use the entire contents of the response as the error text.
+        // Build an ApiError if the response is not a success
         if !response.status().is_success() {
-            let status = response.status();
-            let bytes = response.bytes().await?;
+            tracing::debug!("Failed HTTP request. Status code: {}", response.status());
 
-            tracing::debug!("Failed HTTP request. Status code: {}", status);
-
-            let error_response: ErrorResponse =
-                serde_json::from_slice(&bytes).unwrap_or_else(|_| ErrorResponse::V1ErrorResponse {
-                    error: if bytes.is_empty() {
-                        status
-                            .canonical_reason()
-                            .unwrap_or("Unknown Error")
-                            .to_string()
-                    } else {
-                        String::from_utf8_lossy(&bytes).into_owned()
-                    },
-                    error_description: None,
-                    error_details: None,
-                });
-
-            return Err(Error::ApiError(error_response.into_api_error(status.as_u16())).into());
+            let api_error = api_error_from_response(response).await?;
+            return Err(Error::ApiError(api_error).into());
         }
 
         Ok(response)
     }
 }
 
-/// Error response from TrueLayer APIs.
+/// Body of an error response from TrueLayer APIs.
 #[derive(serde::Deserialize, Debug)]
 #[serde(untagged)]
-enum ErrorResponse {
+enum ErrorResponseBody {
     V3ErrorResponse {
         r#type: String,
         title: String,
@@ -66,41 +51,64 @@ enum ErrorResponse {
         error_description: Option<String>,
         error_details: Option<HashMap<String, String>>,
     },
+    Unknown,
 }
 
-impl ErrorResponse {
-    fn into_api_error(self, http_status: u16) -> ApiError {
-        match self {
-            ErrorResponse::V3ErrorResponse {
-                r#type,
-                title,
-                trace_id,
-                detail,
-                errors,
-            } => ApiError {
-                r#type: Some(r#type),
-                title,
-                status: http_status,
-                trace_id: Some(trace_id),
-                detail: Some(detail),
-                errors: errors.unwrap_or_default(),
-            },
-            ErrorResponse::V1ErrorResponse {
-                error,
-                error_description,
-                error_details,
-            } => ApiError {
-                r#type: None,
-                title: error,
-                status: http_status,
-                trace_id: None,
-                detail: error_description,
-                errors: error_details
-                    .map(|errors| errors.into_iter().map(|(k, v)| (k, vec![v])).collect())
-                    .unwrap_or_default(),
-            },
-        }
-    }
+async fn api_error_from_response(response: Response) -> reqwest_middleware::Result<ApiError> {
+    let status = response.status().as_u16();
+    let tl_correlation_id = response
+        .headers()
+        .get(TL_CORRELATION_ID_HEADER)
+        .map(|v| v.to_str().ok())
+        .flatten()
+        .map(|v| v.to_string());
+
+    // Parse the response body as JSON
+    let bytes = response.bytes().await?;
+    let error_response: ErrorResponseBody =
+        serde_json::from_slice(&bytes).unwrap_or(ErrorResponseBody::Unknown);
+
+    // Map the legacy error versions
+    let api_error = match error_response {
+        ErrorResponseBody::V3ErrorResponse {
+            r#type,
+            title,
+            trace_id,
+            detail,
+            errors,
+        } => ApiError {
+            r#type,
+            title,
+            status,
+            trace_id: Some(trace_id),
+            detail: Some(detail),
+            errors: errors.unwrap_or_default(),
+        },
+        ErrorResponseBody::V1ErrorResponse {
+            error,
+            error_description,
+            error_details,
+        } => ApiError {
+            r#type: "https://docs.truelayer.com/docs/error-types".to_string(),
+            title: error,
+            status,
+            trace_id: tl_correlation_id,
+            detail: error_description,
+            errors: error_details
+                .map(|errors| errors.into_iter().map(|(k, v)| (k, vec![v])).collect())
+                .unwrap_or_default(),
+        },
+        ErrorResponseBody::Unknown => ApiError {
+            r#type: "https://docs.truelayer.com/docs/error-types".to_string(),
+            title: "server_error".to_string(),
+            status,
+            trace_id: tl_correlation_id,
+            detail: None,
+            errors: Default::default(),
+        },
+    };
+
+    Ok(api_error)
 }
 
 #[cfg(test)]
@@ -138,13 +146,17 @@ mod tests {
     async fn json_errors_v1_are_mapped_correctly() {
         let mock_server = MockServer::start().await;
         Mock::given(method("GET"))
-            .respond_with(ResponseTemplate::new(400).set_body_json(json!({
-                "error": "error",
-                "error_description": "description",
-                "error_details": {
-                    "reason": "yes"
-                }
-            })))
+            .respond_with(
+                ResponseTemplate::new(400)
+                    .append_header(TL_CORRELATION_ID_HEADER, "correlation-id")
+                    .set_body_json(json!({
+                        "error": "error",
+                        "error_description": "description",
+                        "error_details": {
+                            "reason": "yes"
+                        }
+                    })),
+            )
             .mount(&mock_server)
             .await;
 
@@ -165,7 +177,10 @@ mod tests {
         };
 
         assert_eq!(api_error.status, 400);
-        assert_eq!(api_error.r#type, None);
+        assert_eq!(
+            api_error.r#type,
+            "https://docs.truelayer.com/docs/error-types"
+        );
         assert_eq!(api_error.title, "error");
         assert_eq!(api_error.detail.as_deref(), Some("description"));
         assert_eq!(
@@ -174,7 +189,7 @@ mod tests {
                 .into_iter()
                 .collect()
         );
-        assert_eq!(api_error.trace_id, None);
+        assert_eq!(api_error.trace_id.as_deref(), Some("correlation-id"));
     }
 
     #[tokio::test]
@@ -213,7 +228,7 @@ mod tests {
         assert_eq!(api_error.status, 400);
         assert_eq!(
             api_error.r#type,
-            Some("https://docs.truelayer.com/docs/error-types#invalid-parameters".to_string())
+            "https://docs.truelayer.com/docs/error-types#invalid-parameters"
         );
         assert_eq!(api_error.title, "Invalid Parameters");
         assert_eq!(api_error.detail.as_deref(), Some("Some more details"));
@@ -230,10 +245,14 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn non_conforming_json_errors_are_treated_as_text() {
+    async fn non_conforming_json_errors_default_to_generic_message() {
         let mock_server = MockServer::start().await;
         Mock::given(method("GET"))
-            .respond_with(ResponseTemplate::new(400).set_body_string("non-conforming error text"))
+            .respond_with(
+                ResponseTemplate::new(400)
+                    .append_header(TL_CORRELATION_ID_HEADER, "correlation-id")
+                    .set_body_string("non-conforming error text"),
+            )
             .mount(&mock_server)
             .await;
 
@@ -254,10 +273,13 @@ mod tests {
         };
 
         assert_eq!(api_error.status, 400);
-        assert_eq!(api_error.r#type, None);
-        assert_eq!(api_error.title, "non-conforming error text");
-        assert_eq!(api_error.detail.as_deref(), None);
+        assert_eq!(
+            api_error.r#type,
+            "https://docs.truelayer.com/docs/error-types"
+        );
+        assert_eq!(api_error.title, "server_error");
+        assert_eq!(api_error.detail, None);
         assert_eq!(api_error.errors, HashMap::new());
-        assert_eq!(api_error.trace_id, None);
+        assert_eq!(api_error.trace_id.as_deref(), Some("correlation-id"));
     }
 }
