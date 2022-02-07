3995d6933c23210201f989ed603f1f61511588b8
Marco Cameriero <marco.cameriero@truelayer.com>
2022-02-07 16:19:29 +0100

    Update domain names and models.

--- truelayer-rust/src/common.rs
+++ truelayer-rust/src/common.rs
@@ -1,9 +1,9 @@
 // Default URLs
 pub static DEFAULT_AUTH_URL: &str = "https://auth.truelayer.com";
-pub static DEFAULT_PAYMENTS_URL: &str = "https://test-pay-api.truelayer.com";
+pub static DEFAULT_PAYMENTS_URL: &str = "https://api.truelayer.com";
 pub static DEFAULT_HOSTED_PAYMENTS_PAGE_URL: &str = "https://payment.truelayer.com";
 pub static DEFAULT_SANDBOX_AUTH_URL: &str = "https://auth.truelayer-sandbox.com";
-pub static DEFAULT_SANDBOX_PAYMENTS_URL: &str = "https://test-pay-api.truelayer-sandbox.com";
+pub static DEFAULT_SANDBOX_PAYMENTS_URL: &str = "https://api.truelayer-sandbox.com";
 pub static DEFAULT_SANDBOX_HOSTED_PAYMENTS_PAGE_URL: &str = "https://payment.truelayer-sandbox.com";
 
 // Header names
